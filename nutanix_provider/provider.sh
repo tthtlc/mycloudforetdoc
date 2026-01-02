93961bdd8bef00b9e572c292295783fccdfcac84
tthtlc <htmldeveloper@gmail.com>
2026-01-03 00:08:56 +0800

    new file:   how_to_docker_build.sh 	renamed:    nutanix/code-samples -> nutanix_code_sample 	new file:   nutanix_provider/provider.sh 	new file:   nutanix_provider/provider.yaml 	new file:   nutanix_provider/schema-service-account.yaml 	new file:   nutanix_provider/secret-singapore.yaml 	new file:   nutanix_provider/service-account-singapore.yaml

---
+++ nutanix_provider/provider.sh
@@ -0,0 +1,7 @@
+
+###delete
+##kubectl exec -n cloudforet deploy/spacectl -- spacectl exec delete identity.Provider -p provider=nutanix
+kubectl exec -n cloudforet deploy/spacectl -- spacectl exec create identity.Provider -f provider.yaml
+kubectl exec -n cloudforet deploy/spacectl -- spacectl exec list identity.Provider
+kubectl exec -n cloudforet deploy/spacectl -- spacectl list identity.Provider
+
